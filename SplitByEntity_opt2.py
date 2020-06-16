ec09ee46531c7acfc3783fbbd7e57a2f8d4d5a92
Svetlana <gruzdevasvetlana@gmail.com>
2020-06-16 14:01:03 +0200

    feat: rename module

--- SplitByEntity_opt2.py
+++ SplitByEntity_opt2.py
@@ -1,9 +1,9 @@
 # SplitByEntity - splits one consolidated file in separate files, one file per entity
 
-import os, logging
+import os, logging, win32com.client
 from tkinter.filedialog import askopenfilename
 
-logging.basicConfig(filename='logs.txt', level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='logs_win32.txt', level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 # logging.disable(logging.CRITICAL)
 
 columnList = []
@@ -18,6 +18,11 @@ if region not in consolFile:
     exit()
 
 # TODO: Get list of entity codes
+xl = win32com.client.Dispatch('Excel.Application')
+# TODO: select file from browser
+wb = xl.Workbooks.Open("C:\\Users\\gruzd\\Documents\\Python_Scripts\\Excel\\Copy_KZ_G&A_planning_template_FCST2_2020.xlsm")
+ws = wb.Worksheets('Summary')
+
 
 # TODO: Remove all unnecessary lines
 
